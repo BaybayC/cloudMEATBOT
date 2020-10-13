00eabda669d5dac31970eade1368acd2e3c326f1
BaybayC <41849568+BaybayC@users.noreply.github.com>
2020-10-13 14:40:02 -0700

    secret command

--- meat_bot.py
+++ meat_bot.py
@@ -21,7 +21,7 @@ async def ping(ctx):
     await ctx.send("Ripped and Ready")
 
 @client.command()
-async def gay(ctx):
+async def weird(ctx):
     await ctx.send("ULTIIIMAATEEECOOWWWLEVELL!!!")
 
 client.run(os.environ['DISCORD_TOKEN'])
