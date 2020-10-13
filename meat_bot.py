e576edacc2d40cae6e61fd1843eee688ff24d743
BaybayC <41849568+BaybayC@users.noreply.github.com>
2020-10-13 13:17:31 -0700

    First Commit

---
+++ meat_bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = ";")
+
+@client.event
+async def on_ready():
+    print("Ready to Int.")
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined the meat gang.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left the meat gang. f in the chat pls.')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send("Ripped and Ready")
+
+client.run('NzY1NDMwODM1MjUxNTExMzA2.X4UtBA.p4Fflhtt_L0s06jER6TBLfEyLdM')
